b33158c9a4d6fe68117187495580a5d58337d831
Jai <jksmithnyc@gmail.com>
2021-09-29 00:55:26 -0400

    completed first section, ui layout and views

---
+++ Shared/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  DartCourses
+//
+//  Created by Jai Smith on 9/29/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(.gray, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Shared/DartCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartCoursesApp.swift
+//  Shared
+//
+//  Created by Jai Smith on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DartCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
